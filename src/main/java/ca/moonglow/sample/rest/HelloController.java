e5ab8ee8c21a442207ca12f068acbff4a5861368
Paul Mooney <pmooney@architech.ca>
2016-03-23 10:24:23 -0400

    first commit

---
+++ src/main/java/ca/moonglow/sample/rest/HelloController.java
@@ -0,0 +1,14 @@
+package ca.moonglow.sample.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
